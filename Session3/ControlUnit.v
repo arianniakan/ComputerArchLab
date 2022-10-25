7ce081191991536ef4f9c76b747d498515fffc55
AmirAGD <aghamohammadi78@gmail.com>
2022-10-25 15:34:14 +0330

    ControlUnit_ConditiocCheck

---
+++ Session3/ControlUnit.v
@@ -0,0 +1,24 @@
+module ControlUnit(input S, input mode [1:0], input Op-code [3:0], output Execute_Command [3:0],
+                   output mem_read, output mem_write,output WB_enable,output B,output Status_Reg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
