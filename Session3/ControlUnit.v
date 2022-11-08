950dbcba3ad8e0612beda49705251005d030c193
arianniakan <65303603+arianniakan@users.noreply.github.com>
2022-11-08 15:57:19 +0330

    Control Unit module
    waiting to be tested

--- Session3/ControlUnit.v
+++ Session3/ControlUnit.v
@@ -1,32 +1,44 @@
 module ControlUnit (input S_in,
                     input [1:0] mode,
                     input [3:0] Op_code, 
-                    output [3:0] Execute_Command,
-                    output mem_read, mem_write, 
+                    output reg [3:0] Execute_Command,
+                    output reg mem_read, mem_write, 
                     WB_enable, B, S_out);
 
-                    localparam [3:0] = ;
-always @(Op_code, mode) begin
+                    localparam [3:0] MOV = 4'b1101, MVN = 4'b1111, ADD = 4'b0100, 
+                                     ADC = 4'b0101, SUB = 4'b0010, SBC = 4'b0110,
+                                     AND = 4'b0000, ORR = 4'b1100, EOR = 4'b0001, 
+                                     CMP = 4'b1010, TST = 4'b1000, LDR = 4'b0100,
+                                     STR = 4'b0100, NOP = 4'b0000;
+always @(Op_code, mode, S_in) begin
+    {Execute_Command, mem_read, mem_write, 
+    WB_enable, B} = 8'b0;
+    S_out = S_in;
     case (mode)
-        2'b00: 
+        2'b00: begin
         case (Op_mode)
-            4'b0001:
-            4'b1111: 
-            4'b0100: 
-            4'b0101: 
-            4'b0010: 
-            4'b0110: 
-            4'b0000: 
-            4'b: 
-            4'b: 
-            4'b: 
- 
-            default: 
+            MOV: begin Execute_Command = 4'b0001; WB_enable = 1'b1; S_out = S_in;end 
+            MVM: begin Execute_Command = 4'b1001; WB_enable = 1'b1; S_out = S_in;end 
+            ADD: begin Execute_Command = 4'b0010; WB_enable = 1'b1; S_out = S_in;end 
+            ADC: begin Execute_Command = 4'b0011; WB_enable = 1'b1; S_out = S_in;end 
+            SUB: begin Execute_Command = 4'b0100; WB_enable = 1'b1; S_out = S_in;end 
+            SBC: begin Execute_Command = 4'b0101; WB_enable = 1'b1; S_out = S_in;end 
+            AND: begin Execute_Command = 4'b0110; WB_enable = 1'b1; S_out = S_in;end 
+            ORR: begin Execute_Command = 4'b0111; WB_enable = 1'b1; S_out = S_in;end 
+            EOR: begin Execute_Command = 4'b1000; WB_enable = 1'b1; S_out = S_in;end 
+            CMP: begin Execute_Command = 4'b0100; S_out = 1'b1; end
+            TST: begin Execute_Command = 4'b0110; S_out = 1'b1; end
+            default: begin {Execute_Command, mem_read, mem_write, 
+            WB_enable, B} = 8'b0;     S_out = S_in; end
         endcase
-        2'b01: 
-        2'b10: 
-        2'b11:
-        default: 
+        end
+        2'b01: begin
+            LDR: begin mem_read = 1'b1; Execute_Command = 4'b0010; WB_enable = 1'b1; S_out = 1'b1;end 
+            STR: begin mem_write = 1'b1; Execute_Command = 4'b0010; S_out = 1'b0;end
+        end
+        2'b10: begin
+            B = 1'b1;
+        end
     endcase
     
 end
