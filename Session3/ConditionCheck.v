55106d2ef331804bf345ccc82d69fca8d8febe42
AmirAGD <aghamohammadi78@gmail.com>
2022-11-08 15:12:04 +0330

    Condition check tested

--- Session3/ConditionCheck.v
+++ Session3/ConditionCheck.v
@@ -1,9 +1,9 @@
 module ConditionCheck(input [3:0] Cond,
                       input Z, C , N, V,
-                      output CondFlag);
+                      output reg CondFlag);
 
-parameter [3:0] EQ = 4'b0000,NE=4'b0001,CS_HS=4'b0010,CC_LO=4'b0011,MI=4'b0100,PL=4'b0101,VS=4'b0110
-                VC=4'b0111,HI=4'b1000,LS=4'b1001,GE=4'b1010,LT=4'b1011,GT=4'b1100,LE=4'b1101,AL=4'b1110 
+parameter [3:0] EQ = 4'b0000,NE=4'b0001,CS_HS=4'b0010,CC_LO=4'b0011,MI=4'b0100,PL=4'b0101,VS=4'b0110,
+                VC=4'b0111 , HI=4'b1000,LS=4'b1001,GE=4'b1010,LT=4'b1011,GT=4'b1100,LE=4'b1101,AL=4'b1110, 
                 NOP=4'b1111;
 
  always @(Cond,Z,C,N,V) begin
@@ -17,14 +17,14 @@ parameter [3:0] EQ = 4'b0000,NE=4'b0001,CS_HS=4'b0010,CC_LO=4'b0011,MI=4'b0100,P
         VS: CondFlag = V ? 1'b1 : 1'b0;
         VC: CondFlag= ~V ? 1'b1 : 1'b0;
         HI: CondFlag= (C&~Z) ? 1'b1 : 1'b0;
-        LS: CondFlag= (~C&Z) ? 1'b1 : 1'b0;
+        LS: CondFlag= (~C|Z) ? 1'b1 : 1'b0;
         GE: CondFlag= ((N&V)|(~N&~V)) ? 1'b1 : 1'b0;
         LT: CondFlag= ((N&~V)|(~N&V)) ? 1'b1 : 1'b0;
         GT: CondFlag= ((~Z&(N|V))|(~N&~V)) ? 1'b1 : 1'b0;
         LE: CondFlag= ((Z|(N&~V))|(~N&V)) ? 1'b1 : 1'b0;
         AL: CondFlag=1'b1;
         NOP: CondFlag=1'b0;
-        default: 
+        default:CondFlag=1'b0; 
     endcase
     
  end   
