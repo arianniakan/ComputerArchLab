b992c27a13c10e015a9c6f2fd24ff2df58427e09
arianniakan <65303603+arianniakan@users.noreply.github.com>
2022-11-08 13:36:11 +0330

    Module Redefined

--- Session3/ControlUnit.v
+++ Session3/ControlUnit.v
@@ -1,24 +1,5 @@
-module ControlUnit(input S, input mode [1:0], input Op-code [3:0], output Execute_Command [3:0],
-                   output mem_read, output mem_write,output WB_enable,output B,output Status_Reg);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module ControlUnit (input S, input [1:0]  mode, input [3:0] Op_code, output [3:0] Execute_Command,
+                   output mem_read, output mem_write,output WB_enable, output B, output Status_Reg);
 
 
 endmodule
