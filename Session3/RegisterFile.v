d8f6224f38760287908729d4f07039260742d6c6
arianniakan <65303603+arianniakan@users.noreply.github.com>
2022-11-08 11:54:24 +0330

    RF cleanup

--- Session3/RegisterFile.v
+++ Session3/RegisterFile.v
@@ -1,8 +1,9 @@
-module RegisterFile( input clk, rst, input [3:0] src1, src2, Dest_eb, 
-                    input [31:0] Result_WB, 
-                    input writeBackEn, 
-                    output [31:0] reg1, reg2);
-reg [31:0] REGISTERS [0:16];
+module RegisterFile( input clk, rst, 
+                     input [3:0] src1, src2, Dest_wb, 
+                     input [31:0] Result_WB, 
+                     input writeBackEn, 
+                     output [31:0] reg1, reg2);
+reg [31:0] REGISTERS [0:14];
 
 
 
